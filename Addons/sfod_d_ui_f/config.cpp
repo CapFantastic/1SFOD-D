032fb3fecb968565da38942ec6006d67e671f0af
Capt. Fantastic <37573050+CapFantastic@users.noreply.github.com>
2021-05-14 13:14:13 +0100

    Add files via upload

---
+++ Addons/sfod_d_ui_f/config.cpp
@@ -0,0 +1,27 @@
+/*=======================================================================================
+Author: Elliot Farrow aka. Captain Fantastic
+License: CC BY-NC-ND
+File Name: config.cpp
+=======================================================================================*/
+
+class CfgPatches 
+{
+	class cf_MOD_ui_f
+	{
+		units[] = 
+		{
+			
+		};
+		weapons[] = 
+		{
+			
+		};
+		requiredVersion = 0.1;
+		requiredAddons[] = 
+		{
+			"cf_MOD_data_f"
+		};
+		author = "Capt. Fantastic";
+		authorUrl = "https://twitter.com/Captain12957861?lang=en";
+	};
+};
